514d5cadc821a838234ab8606589174f215f07f0
Antti Nieminen <anttihn@gmail.com>
2013-03-26 09:37:06 +0200

    deleted old files

--- aceeditor/src/main/java/org/vaadin/aceeditor/SelectionChangeListener.java
+++
@@ -1,5 +0,0 @@
-package org.vaadin.aceeditor;
-
-public interface SelectionChangeListener {
-	public void selectionChanged(int start, int end);
-}
--- aceeditor/src/main/java/org/vaadin/aceeditor/gwt/ace/GwtAceChangeSelectionHandler.java
+++
@@ -1,9 +0,0 @@
-package org.vaadin.aceeditor.gwt.ace;
-
-/**
- * Listens to Ace selection changes.
- * 
- */
-public interface GwtAceChangeSelectionHandler {
-	public void onChangeSelection(GwtAceEvent e);
-}
